34b54f4d944759c35b94e53af27d597816593efd
Roman Hochhalter <romanhochhalter1@web.de>
2024-10-14 16:28:00 +0200

    parse json string

--- main.cpp
+++ main.cpp
@@ -9,17 +9,18 @@ int main() {
     auto  ygoRequestHandler = new CHttpRequestHandler("https://db.ygoprodeck.com/api/v7/cardinfo.php");
     std::string result = std::string("");
 
-    std::cout << "1223" << std::endl;
-
     try {
         result = ygoRequestHandler->Request();
 
-        std::cout << "HTTP Response: " << std::endl << std::endl << result << std::endl;
+        std::cout << "HTTP Request successful!" << std::endl;
     } catch (const std::runtime_error& e) {
         std::cout << e.what() << std::endl;
     }
 
-    // if (jsonReader.parse(result, jsonData))
+    if (jsonReader.parse(result, jsonData)) {
+        // std::cout << jsonData.toStyledString() << std::endl;
+        std::cout << jsonData["data"].size() << std::endl;
+    }
 
     return 0;
 }
